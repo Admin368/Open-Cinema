9056a0f00d827e6f7ded496bd6800c50fa558199
Admin368 <58757588+Admin368@users.noreply.github.com>
2022-03-13 00:07:10 +0800

    +axios

--- room/room_sockets.js
+++ room/room_sockets.js
@@ -1,15 +1,23 @@
 //SOCKET
 import io from 'Socket.IO-client';
-export const socket = io('http://localhost:3000');
+import axios from 'axios';
+export const serverUrl = 'http://localhost:3000';
+export const socket = io(serverUrl, {
+    reconnectionDelayMax: 10000,
+});
+
 
 
 // socket.on('room_0_room',(command)=>{
 //     const roomId = command.id||'';
 //     console.log(command);
 // });
-
 export function socket_request_send(request){
     // console.log('socket_request_send');
     // console.log(request);
     socket.emit('request',request);
 }
+
+socket.io.on('reconnect',()=>{
+    console.log('reconnect');
+});
