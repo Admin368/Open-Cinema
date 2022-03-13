19164dfb46faa840dbae87d3df7b6716c487b9d5
Admin368 <58757588+Admin368@users.noreply.github.com>
2022-03-14 02:14:49 +0800

    Update room_sockets.js

--- room/room_sockets.js
+++ room/room_sockets.js
@@ -2,13 +2,10 @@
 import io from 'Socket.IO-client';
 import axios from 'axios';
 import {cookies_setUserInfo, cookies_getUserInfo} from '../storage/storage.js';
-export const serverUrl = 'http://192.168.1.107:3000';
+export const serverUrl = 'http://192.168.16.109:3000';
 export const socket = io(serverUrl, {
     reconnectionDelayMax: 10000,
 });
-
-
-
 // socket.on('room_0_room',(command)=>{
 //     const roomId = command.id||'';
 //     console.log(command);
@@ -19,6 +16,5 @@ export function socket_request_send(request){
     socket.emit('request',request);
 }
 
-socket.io.on('reconnect',()=>{
-    console.log('reconnect');
-});
+
+
