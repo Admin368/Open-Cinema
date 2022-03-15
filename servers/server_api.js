085003979e248cdcb31e375298133610ae4d3df3
Admin368 <58757588+Admin368@users.noreply.github.com>
2022-03-16 03:00:34 +0800

    successful multi type, multi request Support
    - stress tested api, handling multiple requests without crashing

--- servers/server_api.js
+++ servers/server_api.js
@@ -18,6 +18,7 @@ try{
         res.send('<p>getLink Apis</p>');
     });
 
+    // ZXZJ LEGACY LINK
     app.get('/api/zxzj', cors(corsOptions), async (req, res) => {
         index++;
         var link = req.query.link;
@@ -31,50 +32,32 @@ try{
         }
     });
 
-    app.get('/api/zxzj2', cors(corsOptions), async (req, res) => {
-        try{
-            var link = req.query.link;
-            console.log('API/ZXZJ2-->REQUEST:'+link);
-            
-            if(req.query.link.search("zxzjtv.com/ckplayer.php?")==12){
-                const { Builder } = require('selenium-webdriver');
-                const chrome = require('selenium-webdriver/chrome');
-                const driver = new Builder()
-                .forBrowser('chrome')
-                // .setChromeOptions(option)
-                .build();
-                try{
-                    await driver.get(link);
-                    const getEpisodeUrl = async() => {
-                        var url = await driver.executeScript('return urls');
-                        return url;
-                    }
-                    const url = await getEpisodeUrl();
-                    console.log('API/ZXZJ2-->[SUCCESS]-->LINK:'+url);
-
-                    res.status(200).json(
-                        { 
-                            link: link,
-                            videoUrl: url
-                        }
-                    );
-                }catch(error){
-                    res.status(200).json({ msg: error , link:null});
-                    try{
-                        driver.close();
-                    }catch{
-                        console.log('No Driver');
-                    }
+    // ZXZJ NEW AND LEGACY LINK
+    app.get('/api/general', cors(corsOptions), async (req, res) => {
+        index++;
+        var link = req.query.link;
+        var type = req.query.type;
+        if(!link||!type){res.status(200).json({ msg: 'Empty Link / Type', link:null});return}
+        console.log(index+'_API/ZXZJ-->REQUEST:'+link);
+        switch(type){
+            case 'zxzj':
+                if(req.query.link.search("zxzjtv.com/video/")==12){
+                    const result = await zxzj(link, index);
+                    res.status(200).json(result);
+                }else{
+                    res.status(200).json({ msg: 'Empty Link or Wrong Format', link:null});
                 }
-                driver.quit();
-                // driver.close();
-            }else{
-                res.status(200).json({ msg: 'Empty Link', link:null});
-            }
-        }catch(error){
-            console.log('fatal Error');
-            console.log(error);
-            res.status(200).json({ msg: 'fatal Error', link:null});
+                break;
+            case 'zxzj2':
+                if(req.query.link.search("zxzjtv.com/ckplayer.php?")==12){
+                    const result = await zxzj2(link, index);
+                    res.status(200).json(result);
+                }else{
+                    res.status(200).json({ msg: 'Empty Link or Wrong Format', link:null});
+                }
+                break;
+            default:
+                res.status(200).json({ msg: 'Type Unknown', link:null});
         }
     });
 }catch(error){
@@ -94,7 +77,44 @@ async function zxzj(link, index){
             await drivers[index].get(link);
             const getEpisodeUrl = async() => {
                 var player_data = await drivers[index].executeScript('return player_data');
-                return player_data.url;
+                return player_data.url;       
+            }
+            const url = await getEpisodeUrl();
+            console.log('API/ZXZJ2-->[SUCCESS]-->LINK:'+url);
+            drivers[index].close();
+            return { 
+                link: link,
+                videoUrl: url
+            }
+        }catch(error){
+            try{
+                drivers[index].close();
+                const errorMsg =`REQUEST:${index} ERROR1: Failed to getVideo Link, Check link`;
+                console.log(errorMsg);
+                // console.log(error);
+                return{ msg: errorMsg , link:null};
+            }catch{
+                const errorMsg =`REQUEST:${index} ERROR2: No Driver`;
+                console.log(errorMsg);
+                return{ msg: errorMsg, link:null};
+            }
+        }
+        drivers[index].close();
+}
+
+async function zxzj2(link, index){
+    console.log(index+'ACCEPTED ');
+    try{
+        const { Builder } = require('selenium-webdriver');
+        const chrome = require('selenium-webdriver/chrome');
+        drivers[index] = new Builder()
+        .forBrowser('chrome')
+        // .setChromeOptions(option)
+        .build();
+            await drivers[index].get(link);
+            const getEpisodeUrl = async() => {
+                var urls = await drivers[index].executeScript('return urls');
+                return urls;
             }
             const url = await getEpisodeUrl();
             console.log('API/ZXZJ2-->[SUCCESS]-->LINK:'+url);
