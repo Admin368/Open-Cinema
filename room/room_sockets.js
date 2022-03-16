994d65e2ef23e533ad59d6c99963bc1a2c75cf5c
Admin368 <58757588+Admin368@users.noreply.github.com>
2022-03-17 01:33:01 +0800

    server-name change

--- room/room_sockets.js
+++ room/room_sockets.js
@@ -10,7 +10,7 @@ export const port=5001;
 export const socketUrl=hostUrl+':'+port;
 // export const socket = io('https://2ae5-152-101-158-174.ngrok.io', {
 // export const socket = io(socketUrl, {
-export const socket = io('https://pacific-badlands-27887.herokuapp.com/', {
+export const socket = io('https://movie-knight-server.herokuapp.com/', {
     reconnectionDelayMax: 10000,
 });
 // socket.on('room_0_room',(command)=>{
