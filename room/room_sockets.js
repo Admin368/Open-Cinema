dcbcfddce7b9661171de9c1d581161d9b656812a
Admin368 <58757588+Admin368@users.noreply.github.com>
2022-03-16 11:18:06 +0800

    export server info

--- room/room_sockets.js
+++ room/room_sockets.js
@@ -2,8 +2,12 @@
 import io from 'Socket.IO-client';
 import axios from 'axios';
 import {cookies_setUserInfo, cookies_getUserInfo} from '../storage/storage.js';
-export const serverUrl = 'http://192.168.16.109:3000';
-export const socket = io(serverUrl, {
+export const hostUrl = 'http://192.168.16.109';// shahe
+// export const serverUrl = '172.16.10.178';// h
+// export const serverUrl = 'localhost';// ngrok
+export const port=3001;
+export const socketUrl=hostUrl+':'+port;
+export const socket = io(socketUrl, {
     reconnectionDelayMax: 10000,
 });
 // socket.on('room_0_room',(command)=>{
