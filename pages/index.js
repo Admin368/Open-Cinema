d3383797cef98f8d88849e9146336fa80c531361
Admin368 <58757588+Admin368@users.noreply.github.com>
2022-03-20 18:40:43 +0800

    new page updates

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
+import { re } from "mathjs";
 import React from "react";
 
 import AppLayout from "../layouts/layout2/AppLayout";
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
+
 import './style.less';
+// import video from '../video/frontPage.mp4';
+// const video = '../assets/video.mp4';
 
 const index = props => {
   return (
     <AppLayout 
-      // showLogo={false}
+      showLogo={false}
       showCrumbs={false}
       showMenus={false}
-      // showFooter={false}
+      showFooter={false}
+      showVideoBackground={true}
+      videoBackgroundUrl='bgVideo_Home.mp4'
     >
-      <div className="text">Sample Page</div>
+    <div className='page_index'>Sample Page</div>
+    <Footer  className='footer home_footer' style={{ textAlign: 'center', textJustify: 'center' }}>U-byte Devs and Admin368 ©2022</Footer>
     </AppLayout>
   );
 };
